c2f5fb26a8ecd8a09edc678c881251da11618233
Dewey <hammersdrichard@gmail.com>
2021-01-23 09:20:51 -0800

    add a module for cli

--- lib/star_wars_cli/cli.rb
+++ lib/star_wars_cli/cli.rb
@@ -1,15 +1,17 @@
 class Cli
+    include UserCommands
+
     def call
         puts "---------------------------------------------------------------------------"
         puts "| Welcome! This is where you can find all things on everyhting Star Wars! |"
         puts "---------------------------------------------------------------------------"
-        print_categorys()
+        self.print_categorys()
         input = nil
         while input != "exit" 
             input = gets.chomp
             list = StarWars.get_category_names(input)
             if list
-                print_category_names(list)
+                self.print_category_names(list)
                 status = nil
                 while status != true 
                     input = gets.chomp
@@ -32,46 +34,20 @@ class Cli
                     else
                         if input == 'back'
                             status = true
-                            print_categorys()
+                            self.print_categorys()
                         elsif input == "list"
-                            print_category_names(list)
+                            self.print_category_names(list)
                         elsif input == 'exit'
                             status = true
                         elsif input != true
-                            invaild()
+                            self.invaild()
                         end
                     end
                 end
             elsif input != "exit" 
-                invaild()
+                self.invaild()
             end
         end
         puts "Good bye! May the force be with you!"
     end 
-
-    def print_categorys
-        puts "---------------"
-        puts "|1. People    |"  
-        puts "|2. Planets   |" 
-        puts "|3. Films     |"   
-        puts "|4. Species   |"
-        puts "|5. Vehicles  |"
-        puts "|6. Starships |"
-        puts "---------------"
-        puts "Choose a number from the category you would like to look up or 'exit' to leave the app."
-        print "Enter a command: "
-    end
-    
-    def print_category_names(list)
-        puts "---------------------"
-        puts list
-        puts "---------------------"
-        puts "Please enter a number to choose from the list of names or 'back' to go back or 'exit' to leave the app."
-        print "Enter a command: "
-    end
-    
-    def invaild
-        puts "Invaild!"
-        print "Please re-enter a command: "
-    end
 end
---
+++ lib/star_wars_cli/concerns/cli_user_info.rb
@@ -0,0 +1,27 @@
+module UserCommands
+    def print_categorys
+        puts "---------------"
+        puts "|1. People    |"  
+        puts "|2. Planets   |" 
+        puts "|3. Films     |"   
+        puts "|4. Species   |"
+        puts "|5. Vehicles  |"
+        puts "|6. Starships |"
+        puts "---------------"
+        puts "Choose a number from the category you would like to look up or 'exit' to leave the app."
+        print "Enter a command: "
+    end
+    
+    def print_category_names(list)
+        puts "---------------------"
+        puts list
+        puts "---------------------"
+        puts "Please enter a number to choose from the list of names or 'back' to go back or 'exit' to leave the app."
+        print "Enter a command: "
+    end
+    
+    def invaild
+        puts "Invaild!"
+        print "Please re-enter a command: "
+    end
+end
